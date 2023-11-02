f33e6394476cc3948628747cfe7afacadb85e705
Olek <dygons14@gmail.com>
2023-11-02 15:42:07 +0100

    Configured tests

--- src/contracts/logic/math.rs
+++ src/contracts/logic/math.rs
@@ -113,21 +113,18 @@ pub fn get_liquidity_by_x_sqrt_price(
 
     if current_sqrt_price < lower_sqrt_price {
         // Checked_from_decimal_to_value
-        let nominator = (lower_sqrt_price * upper_sqrt_price)
-            .checked_div(SqrtPrice::new(PRICE_DENOMINATOR))
-            .unwrap();
+        let nominator =
+            (lower_sqrt_price.big_mul(upper_sqrt_price)).big_div(SqrtPrice::new(PRICE_DENOMINATOR));
         let denominator = upper_sqrt_price - lower_sqrt_price;
-        let liquidity =
-            Liquidity::new((x.0 * nominator.get() * LIQUIDITY_DENOMINATOR) / denominator.get());
+        let liquidity = Liquidity::from_integer((x.0 * nominator.get()) / denominator.get());
         return (liquidity, TokenAmount(0));
     }
 
-    let nominator = lower_sqrt_price
+    let nominator = current_sqrt_price
         .big_mul(upper_sqrt_price)
         .big_div(SqrtPrice::new(PRICE_DENOMINATOR));
     let denominator = upper_sqrt_price - current_sqrt_price;
-    let liquidity =
-        Liquidity::new((x.0 * nominator.get() * LIQUIDITY_DENOMINATOR) / denominator.get());
+    let liquidity = Liquidity::from_integer((x.0 * nominator.get()) / denominator.get());
     let sqrt_price_diff = current_sqrt_price - lower_sqrt_price;
     let y = calculate_y(sqrt_price_diff, liquidity, rounding_up);
     (liquidity, y)
@@ -174,14 +171,12 @@ pub fn get_liquidity_by_y_sqrt_price(
 
     if upper_sqrt_price <= current_sqrt_price {
         let sqrt_price_diff = upper_sqrt_price - lower_sqrt_price;
-        let liquidity =
-            Liquidity::new(y.0 * LIQUIDITY_DENOMINATOR * PRICE_DENOMINATOR / sqrt_price_diff.get());
+        let liquidity = Liquidity::from_integer(y.0 * PRICE_DENOMINATOR / sqrt_price_diff.get());
         return (liquidity, TokenAmount::new(0));
     }
 
     let sqrt_price_diff = current_sqrt_price - lower_sqrt_price;
-    let liquidity =
-        Liquidity::new(y.0 * LIQUIDITY_DENOMINATOR * PRICE_DENOMINATOR / sqrt_price_diff.get());
+    let liquidity = Liquidity::from_integer(y.0 * PRICE_DENOMINATOR / sqrt_price_diff.get());
     let denominator =
         (current_sqrt_price.big_mul(upper_sqrt_price)).big_div(SqrtPrice::new(PRICE_DENOMINATOR));
     let nominator = upper_sqrt_price - current_sqrt_price;
@@ -197,10 +192,10 @@ pub fn calculate_x(
     liquidity: Liquidity,
     rounding_up: bool,
 ) -> TokenAmount {
-    let common = liquidity.get() * nominator.get() / denominator.get();
+    let common = liquidity.big_mul(nominator).big_div(denominator).get();
 
     if rounding_up {
-        TokenAmount::new(common + LIQUIDITY_DENOMINATOR - 1 / LIQUIDITY_DENOMINATOR)
+        TokenAmount::new(((common + LIQUIDITY_DENOMINATOR) - 1) / LIQUIDITY_DENOMINATOR)
     } else {
         TokenAmount::new(common / LIQUIDITY_DENOMINATOR)
     }
@@ -211,15 +206,14 @@ pub fn calculate_y(
     liquidity: Liquidity,
     rounding_up: bool,
 ) -> TokenAmount {
-    let shifted_liquidity = liquidity / Liquidity::from_integer(1);
+    let shifted_liquidity = liquidity.get() / LIQUIDITY_DENOMINATOR;
     if rounding_up {
-        // there should be a decimal functiion to mul_up
         TokenAmount::new(
-            sqrt_price_diff.get() * shifted_liquidity.get()
-                + (PRICE_DENOMINATOR - 1) / PRICE_DENOMINATOR,
+            ((sqrt_price_diff.get() * shifted_liquidity) + (PRICE_DENOMINATOR - 1))
+                / PRICE_DENOMINATOR,
         )
     } else {
-        TokenAmount::new(sqrt_price_diff.get() * shifted_liquidity.get() / PRICE_DENOMINATOR)
+        TokenAmount::new(sqrt_price_diff.get() * shifted_liquidity / PRICE_DENOMINATOR)
     }
 }
 
@@ -248,27 +242,109 @@ mod tests {
 
     #[test]
     fn get_liquidity_by_x_test() {
-        let expected_liquidity: Liquidity;
-        let expected_y: TokenAmount;
-        let x = TokenAmount(1);
-        let lower_tick = -10;
-        let upper_tick = 10;
-        let current_sqrt_price = SqrtPrice::from_integer(1);
-        let result = get_liquidity_by_x(x, lower_tick, upper_tick, current_sqrt_price, true, 10);
-        println!("Result = {:?}", result);
-        // Result = (Liquidity { v: 199960000000 }, TokenAmount(99950012998600000000000000000000))
+        let x = TokenAmount::new(43_0000);
+        let current_sqrt_price = calculate_sqrt_price(100).unwrap();
+        // below current tick
+        {
+            let lower_tick = -50;
+            let upper_tick = 10;
+            let result = get_liquidity_by_x(x, lower_tick, upper_tick, current_sqrt_price, true, 1);
+            let expected_l = Liquidity::new(0);
+            let expected_y = TokenAmount(0);
+            assert_eq!(result.0, expected_l);
+            assert_eq!(result.1, expected_y);
+        }
+        // in current tick
+        {
+            let expected_l = Liquidity::new(432392997000000);
+            let expected_y_up = TokenAmount(434322);
+            let expected_y_down = TokenAmount(434321);
+
+            let lower_tick = 80;
+            let upper_tick = 120;
+
+            let result_up =
+                get_liquidity_by_x(x, lower_tick, upper_tick, current_sqrt_price, true, 1);
+
+            let result_down =
+                get_liquidity_by_x(x, lower_tick, upper_tick, current_sqrt_price, false, 1);
+            assert_eq!(expected_l, result_up.0);
+            assert_eq!(expected_y_up, result_up.1);
+            assert_eq!(expected_l, result_down.0);
+            assert_eq!(expected_y_down, result_down.1);
+        }
+        // above current tick
+        {
+            let expected_l = Liquidity::new(13548826000000);
+            let expected_y = TokenAmount(0);
+            let lower_tick = 150;
+            let upper_tick = 800;
+
+            let result_up =
+                get_liquidity_by_x(x, lower_tick, upper_tick, current_sqrt_price, true, 1);
+            assert_eq!(expected_l, result_up.0);
+            assert_eq!(expected_y, result_up.1);
+
+            let result_down =
+                get_liquidity_by_x(x, lower_tick, upper_tick, current_sqrt_price, false, 1);
+            assert_eq!(expected_l, result_down.0);
+            assert_eq!(expected_y, result_up.1);
+        }
     }
 
     #[test]
     fn get_liquidity_by_y_test() {
-        let expected_liquidity: Liquidity;
-        let expected_x: TokenAmount;
-        let y = TokenAmount(1);
-        let lower_tick = -10;
-        let upper_tick = 10;
-        let current_sqrt_price = SqrtPrice::from_integer(1);
-        let result = get_liquidity_by_y(y, lower_tick, upper_tick, current_sqrt_price, true, 10);
-        println!("Result = {:?}", result);
+        let y = TokenAmount(476_000_000_00);
+        let current_sqrt_price = calculate_sqrt_price(-20000).unwrap();
+        // below current tick
+        {
+            let expected_l = Liquidity::new(2789052279103000000); // PROTOCOL = 2789052279103923275
+            let expected_x = TokenAmount(0);
+            let lower_tick = -22000;
+            let upper_tick = -21000;
+
+            let result_up =
+                get_liquidity_by_y(y, lower_tick, upper_tick, current_sqrt_price, true, 1);
+            assert_eq!(expected_l, result_up.0);
+            assert_eq!(expected_x, result_up.1);
+            let result_down =
+                get_liquidity_by_y(y, lower_tick, upper_tick, current_sqrt_price, false, 1);
+            assert_eq!(expected_l, result_down.0);
+            assert_eq!(expected_x, result_down.1);
+        }
+        // in current tick
+        {
+            let expected_l = Liquidity::new(584945290554000000); // PROTOCOL = 584945290554346935
+            let expected_x_up = TokenAmount(77539808126);
+            let expected_x_down = TokenAmount(77539808125);
+            let lower_tick = -25000;
+            let upper_tick = -19000;
+
+            let result_up =
+                get_liquidity_by_y(y, lower_tick, upper_tick, current_sqrt_price, true, 1);
+            assert_eq!(expected_l, result_up.0);
+            assert_eq!(expected_x_up, result_up.1);
+            let result_down =
+                get_liquidity_by_y(y, lower_tick, upper_tick, current_sqrt_price, false, 1);
+            assert_eq!(expected_l, result_down.0);
+            assert_eq!(expected_x_down, result_down.1);
+        }
+        // above current tick
+        {
+            let expected_l = Liquidity::new(0); // PROTOCOL = 584945290554346935
+            let expected_x = TokenAmount(0);
+            let lower_tick = -10000;
+            let upper_tick = 0;
+
+            let result_up =
+                get_liquidity_by_y(y, lower_tick, upper_tick, current_sqrt_price, true, 1);
+            assert_eq!(expected_l, result_up.0);
+            assert_eq!(expected_x, result_up.1);
+            let result_down =
+                get_liquidity_by_y(y, lower_tick, upper_tick, current_sqrt_price, false, 1);
+            assert_eq!(expected_l, result_down.0);
+            assert_eq!(expected_x, result_down.1);
+        }
     }
     #[test]
     fn get_liquidity_test() {
