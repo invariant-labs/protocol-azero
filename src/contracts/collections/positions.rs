2c9d3a0d1e1323367187697cf3aca7fb194763bf
zielvna <zielvna@gmail.com>
2023-11-27 10:02:12 +0100

    remove unnecessary code and imports from positions

--- src/contracts/collections/positions.rs
+++ src/contracts/collections/positions.rs
@@ -1,9 +1,5 @@
 use crate::{contracts::Position, InvariantError};
-use ink::{
-    prelude::{vec, vec::Vec},
-    primitives::AccountId,
-    storage::Mapping,
-};
+use ink::{primitives::AccountId, storage::Mapping};
 
 #[ink::storage_item]
 #[derive(Debug, Default)]
@@ -11,8 +7,6 @@ pub struct Positions {
     positions_length: Mapping<AccountId, u32>,
     positions: Mapping<(AccountId, u32), Position>,
 }
-
-impl scale::EncodeLike<Vec<Position>> for Position {}
 
 impl Positions {
     pub fn add(&mut self, account_id: AccountId, position: Position) {
