015f4723be61a20d7f94cf03f8554e3f1c40ec54
zielvna <zielvna@gmail.com>
2023-12-04 11:50:37 +0100

    move tests

--- src/tests/entrypoints/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod add_fee_tier;
-pub mod remove_fee_tier;
-
-pub use add_fee_tier::*;
-pub use remove_fee_tier::*;
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,5 +1,9 @@
-pub mod entrypoints;
-pub mod integration;
+pub mod add_fee_tier;
+pub mod cross_both_side;
+pub mod liquidity_gap;
+pub mod remove_fee_tier;
 
-pub use entrypoints::*;
-pub use integration::*;
+pub use add_fee_tier::*;
+pub use cross_both_side::*;
+pub use liquidity_gap::*;
+pub use remove_fee_tier::*;
