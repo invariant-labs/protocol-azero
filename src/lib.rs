b4961d0a7690963dbafed3d7e30c3f20519a27d4
Wojciech <cichockiwojciech97@gmail.com>
2023-12-05 10:15:11 +0100

    Move e2e

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@
 
 extern crate alloc;
 mod contracts;
-pub mod math;
 pub mod e2e;
+pub mod math;
 
 #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
 #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
