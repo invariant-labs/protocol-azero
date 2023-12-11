a9abce2c83baeb9fb663ce10d6539e1cbcd25c9a
Sniezka <dygons14@gmail.com>
2023-12-11 11:36:07 +0100

    Added tests to validate tick and price relations

--- src/e2e/create_pool.rs
+++ src/e2e/create_pool.rs
@@ -4,7 +4,7 @@ pub mod e2e_tests {
         contract::ContractRef,
         contracts::{entrypoints::Invariant, FeeTier},
         math::types::percentage::Percentage,
-        math::types::sqrt_price::calculate_sqrt_price,
+        math::types::sqrt_price::{calculate_sqrt_price, SqrtPrice},
         InvariantError,
     };
     use decimal::*;
@@ -174,6 +174,100 @@ pub mod e2e_tests {
         );
 
         assert_eq!(result, Err(InvariantError::InvalidInitTick));
+
+        Ok(())
+    }
+
+    #[ink_e2e::test]
+    async fn test_create_pool_init_sqrt_price_minimal_difference_from_tick(
+        mut client: ink_e2e::Client<C, E>,
+    ) -> E2EResult<()> {
+        let dex = create_dex!(client, ContractRef, Percentage::new(0));
+        let (token_x, token_y) = create_tokens!(client, TokenRef, 500, 500);
+        let alice = ink_e2e::alice();
+
+        let fee_tier = FeeTier::new(Percentage::from_scale(5, 1), 3).unwrap();
+        let init_tick = 0;
+        let init_sqrt_price = calculate_sqrt_price(init_tick).unwrap() - SqrtPrice::new(1);
+        add_fee_tier!(client, ContractRef, dex, fee_tier, alice).unwrap();
+
+        create_pool!(
+            client,
+            ContractRef,
+            dex,
+            token_x,
+            token_y,
+            fee_tier,
+            init_sqrt_price,
+            init_tick,
+            alice
+        )
+        .unwrap();
+
+        Ok(())
+    }
+
+    #[ink_e2e::test]
+    async fn test_create_pool_init_sqrt_price_has_closer_init_tick(
+        mut client: ink_e2e::Client<C, E>,
+    ) -> E2EResult<()> {
+        let dex = create_dex!(client, ContractRef, Percentage::new(0));
+        let (token_x, token_y) = create_tokens!(client, TokenRef, 500, 500);
+        let alice = ink_e2e::alice();
+
+        let fee_tier = FeeTier::new(Percentage::from_scale(5, 1), 1).unwrap();
+        let init_tick = 2;
+        // tick = 3 -> 1.000150003749000000000000
+        // between  -> 1.000175003749000000000000
+        // tick = 4 -> 1.000200010000000000000000
+        let init_sqrt_price = SqrtPrice::new(1000175003749000000000000);
+        add_fee_tier!(client, ContractRef, dex, fee_tier, alice).unwrap();
+
+        let result = create_pool!(
+            client,
+            ContractRef,
+            dex,
+            token_x,
+            token_y,
+            fee_tier,
+            init_sqrt_price,
+            init_tick,
+            alice
+        );
+        assert_eq!(result, Err(InvariantError::InvalidInitSqrtPrice));
+
+        Ok(())
+    }
+
+    #[ink_e2e::test]
+    async fn test_create_pool_init_sqrt_price_has_closer_init_tick_spacing_over_one(
+        mut client: ink_e2e::Client<C, E>,
+    ) -> E2EResult<()> {
+        let dex = create_dex!(client, ContractRef, Percentage::new(0));
+        let (token_x, token_y) = create_tokens!(client, TokenRef, 500, 500);
+        let alice = ink_e2e::alice();
+
+        let fee_tier = FeeTier::new(Percentage::from_scale(5, 1), 3).unwrap();
+        let init_tick = 3;
+        // tick = 3 -> 1.000150003749000000000000
+        // between  -> 1.000225003749000000000000
+        // tick = 6 -> 1.000300030001000000000000
+        let init_sqrt_price = SqrtPrice::new(1000225003749000000000000);
+        println!("init_sqrt_price: {}", init_sqrt_price);
+        add_fee_tier!(client, ContractRef, dex, fee_tier, alice).unwrap();
+
+        let result = create_pool!(
+            client,
+            ContractRef,
+            dex,
+            token_x,
+            token_y,
+            fee_tier,
+            init_sqrt_price,
+            init_tick,
+            alice
+        );
+        assert_eq!(result, Err(InvariantError::InvalidInitSqrtPrice));
 
         Ok(())
     }
