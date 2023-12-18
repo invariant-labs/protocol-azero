702beb24ceb8b62deb73a10a28d88f2aea68878d
Sniezka <dygons14@gmail.com>
2023-12-18 14:44:29 +0100

    Fixed typo

--- scripts/update_contract_addresses.sh
+++ scripts/update_contract_addresses.sh
@@ -1,4 +1,4 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
 
 # This script copies `addresses.json` files from ./scriptes directory into frontend metadata.
 # This is required so that the frontend can interact with deployed contracts.
