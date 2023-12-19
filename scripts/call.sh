074200b1b1e00e084d2aaba974d1df86056340b7
Sniezka <dygons14@gmail.com>
2023-12-19 19:23:30 +0100

    Added contract call script

--- scripts/call.sh
+++ scripts/call.sh
@@ -3,16 +3,12 @@ SEED="//Alice"
 URL="wss://ws.test.azero.dev"
 CONTRACT="5DNQ2JCwGip6iUuHgam2Y7FZHbJfJsr8M9AQCdVzkBV9LMSM"
 
-ABI_PATH=$(pwd)/target/ink/contract.json
-echo "ABI path: ${ABI_PATH}"
-
 cargo contract call --suri "$SEED" --url "$URL" \
         --contract $CONTRACT \
-        --message new \
-        --args 0 \
-        --output-json \
-        --skip-confirm \
-        --execute
+        --message Invariant::get_protocol_fee \
+        --execute \
+        --skip-confirm
+        
         
 # cargo contract call ABI_PATH --suri "$SEED" --url "$URL" \
 #         --contract 5DNQ2JCwGip6iUuHgam2Y7FZHbJfJsr8M9AQCdVzkBV9LMSM \
