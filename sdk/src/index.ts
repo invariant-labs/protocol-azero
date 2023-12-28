d671198e5691b1d6d5a07a68afdcb2fa1c27c957
zielvna <zielvna@gmail.com>
2023-12-28 16:29:41 +0100

    install eslint and create eslint and prettier configs

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,43 +1,48 @@
-import dotenv from 'dotenv'
-import { Invariant } from './invariant.js'
-import { getDeploymentData, getEnvAccount, initPolkadotApi, printBalance } from './utils.js'
-import { Network } from './network.js'
-import { Keyring } from '@polkadot/api'
-dotenv.config()
+import { Keyring } from "@polkadot/api";
+import dotenv from "dotenv";
+import { Invariant } from "./invariant.js";
+import { Network } from "./network.js";
+import {
+  getDeploymentData,
+  getEnvAccount,
+  initPolkadotApi,
+  printBalance,
+} from "./utils.js";
+dotenv.config();
 
 const main = async () => {
-  const network = Network.getFromEnv()
-  console.log(`Using ${network}`)
-  const api = await initPolkadotApi(network)
-  const keyring = new Keyring({ type: 'sr25519' })
-  const account = await getEnvAccount(keyring)
-  await printBalance(api, account)
+  const network = Network.getFromEnv();
+  console.log(`Using ${network}`);
+  const api = await initPolkadotApi(network);
+  const keyring = new Keyring({ type: "sr25519" });
+  const account = await getEnvAccount(keyring);
+  await printBalance(api, account);
 
-  const { abi, wasm } = await getDeploymentData()
-  const invariant = new Invariant(api, account, network)
+  const { abi, wasm } = await getDeploymentData();
+  const invariant = new Invariant(api, account, network);
 
-  let initFee = { v: 10 }
-  const deployContract = await invariant.deploy(abi, wasm, initFee)
-  await invariant.load(deployContract.address, abi)
+  let initFee = { v: 10 };
+  const deployContract = await invariant.deploy(abi, wasm, initFee);
+  await invariant.load(deployContract.address, abi);
 
-  let initialFee = await invariant.getProtocolFee()
-  console.log(initialFee)
+  let initialFee = await invariant.getProtocolFee();
+  console.log(initialFee);
 
   let newFeeStruct = {
-    v: 100
-  }
+    v: 100,
+  };
 
-  console.log(`Changing protocol fee to: ${newFeeStruct.v}`)
+  console.log(`Changing protocol fee to: ${newFeeStruct.v}`);
 
-  let txHash = await invariant.changeProtocolFee(newFeeStruct)
+  let txHash = await invariant.changeProtocolFee(newFeeStruct);
 
-  console.log('Received txHash  = ', txHash)
+  console.log("Received txHash  = ", txHash);
 
-  let newFee = await invariant.getProtocolFee()
-  console.log(newFee)
+  let newFee = await invariant.getProtocolFee();
+  console.log(newFee);
 
-  console.log('Passed.')
-  process.exit(0)
-}
+  console.log("Passed.");
+  process.exit(0);
+};
 
-main()
+main();
--- sdk/src/network.ts
+++ sdk/src/network.ts
@@ -1,19 +1,19 @@
 export enum Network {
-  Local = 'local',
-  Testnet = 'testnet'
+  Local = "local",
+  Testnet = "testnet",
 }
 
 export namespace Network {
   export function valueOf(str: string): Network {
-    const capitalize = str[0].toUpperCase() + str.slice(1)
-    return Network[capitalize as keyof typeof Network] as Network
+    const capitalize = str[0].toUpperCase() + str.slice(1);
+    return Network[capitalize as keyof typeof Network] as Network;
   }
 
   export function getFromEnv(): Network {
-    let network = Network.valueOf(process.argv[2])
+    let network = Network.valueOf(process.argv[2]);
     if (!network) {
-      throw new Error('network not found')
+      throw new Error("network not found");
     }
-    return network
+    return network;
   }
 }
