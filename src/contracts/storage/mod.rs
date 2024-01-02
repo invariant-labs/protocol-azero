6b4423aaa874a21aadf0a593319a10b6005342a4
zielvna <zielvna@gmail.com>
2024-01-02 10:55:46 +0100

    remove oracle

--- src/contracts/storage/mod.rs
+++ src/contracts/storage/mod.rs
@@ -1,6 +1,5 @@
 pub mod fee_tier;
 pub mod invariant_config;
-pub mod oracle;
 pub mod pool;
 pub mod pool_key;
 pub mod position;
@@ -9,7 +8,6 @@ pub mod tickmap;
 
 pub use fee_tier::*;
 pub use invariant_config::*;
-pub use oracle::*;
 pub use pool::*;
 pub use pool_key::*;
 pub use position::*;
