2aa353f5ab1928e93b3c43eb4d4c25d8bd37247a
zielvna <zielvna@gmail.com>
2024-01-02 11:31:43 +0100

    update tests script

--- tests.sh
+++ tests.sh
@@ -13,7 +13,6 @@ cd traceable_result
 cargo test
 cd ..
 
-
 # Test decimal
 cd decimal
 cargo test
@@ -25,6 +24,14 @@ cargo clippy --all-targets -- --no-deps -D warnings
 
 cargo test --features e2e-tests
 
-# build contract
+# Build contract
 cargo contract build
 cargo contract build --release
+
+# Build and test SDK
+cd sdk
+npm i
+npm run lint
+npm run wasm:build
+npm run build
+./tests.sh
