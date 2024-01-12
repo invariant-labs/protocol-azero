4e8d1ee488439cc62c45d2e5444b626016b0dd1d
Sniezka <dygons14@gmail.com>
2024-01-12 16:29:24 +0100

    Configured generating wrappers for functions that return Result

--- sdk/math/wasm_wrapper/src/lib.rs
+++ sdk/math/wasm_wrapper/src/lib.rs
@@ -2,31 +2,46 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn, ReturnType, Type};
+use syn::spanned::Spanned;
+use syn::{parse_macro_input, FnArg, ItemFn};
 use wasm_bindgen::prelude::*;
 
 #[proc_macro_attribute]
 pub fn wasm_wrapper(_attr: TokenStream, input: TokenStream) -> TokenStream {
-    // Parse the input tokens into a syntax tree
     let input = parse_macro_input!(input as ItemFn);
-
-    let original_function_block = &input.block;
     let original_function_name = &input.sig.ident;
     let visibility = &input.vis;
-    let generated_function_name = format!("{}_generated", original_function_name);
+
+    let generated_function_name = format!("wrapped_{}", original_function_name);
     let generated_function_ident =
         syn::Ident::new(&generated_function_name, original_function_name.span());
-    let original_return_type = match &input.sig.output {
-        ReturnType::Default => quote! { () },
-        ReturnType::Type(_, ty) => quote! { #ty },
+
+    let camel_case_string = {
+        let mut camel_case = String::new();
+        let mut capitalize_next = false;
+        for c in original_function_name.to_string().chars() {
+            if c == '_' {
+                capitalize_next = true;
+            } else {
+                if capitalize_next {
+                    camel_case.push(c.to_ascii_uppercase());
+                    capitalize_next = false;
+                } else {
+                    camel_case.push(c);
+                }
+            }
+        }
+        camel_case
     };
 
     let params = input.sig.inputs.iter().filter_map(|param| {
         if let syn::FnArg::Typed(pat_type) = param {
             if let syn::Pat::Ident(ident) = &*pat_type.pat {
                 let param_name = &ident.ident;
+                let js_param_name =
+                    syn::Ident::new(&format!("js_{}", param_name), ident.ident.span());
                 let param_ty = quote! { wasm_bindgen::JsValue };
-                Some(quote! { #param_name: #param_ty })
+                Some(quote! { #js_param_name: #param_ty })
             } else {
                 None
             }
@@ -35,15 +50,44 @@ pub fn wasm_wrapper(_attr: TokenStream, input: TokenStream) -> TokenStream {
         }
     });
 
-    // Generate the new function with the specified name
+    let (conversion_code, converted_params): (Vec<_>, Vec<_>) = input
+        .sig
+        .inputs
+        .iter()
+        .map(|param| {
+            if let FnArg::Typed(pat_type) = param {
+                if let syn::Pat::Ident(ident) = &*pat_type.pat {
+                    let param_name = &ident.ident;
+                    let js_param_name =
+                        syn::Ident::new(&format!("js_{}", param_name), ident.span());
+                    let param_ty = &pat_type.ty;
+                    (
+                        quote! { let #param_name: #param_ty = serde_wasm_bindgen::from_value(#js_param_name).unwrap(); },
+                        quote! { #param_name },
+                    )
+                } else {
+                    (quote! {}, quote! {})
+                }
+            } else {
+                (quote! {}, quote! {})
+            }
+        })
+        .unzip();
+
     let new_function = quote! {
-        #[wasm_bindgen]
-        #visibility fn #generated_function_ident(#(#params),*) {
-            // #original_function_block
+        #[wasm_bindgen(js_name = #camel_case_string)]
+        #visibility fn #generated_function_ident(#(#params),*) -> Result<JsValue, JsValue> {
+            #(#conversion_code)*
+
+            let result = #original_function_name(#(#converted_params),*);
+
+            match result {
+                Ok(v) => Ok(serde_wasm_bindgen::to_value(&v)?),
+                Err(error) => Err(JsValue::from_str(&error.to_string())),
+            }
         }
     };
 
-    // Combine the original function with the generated function
     let result = quote! {
         #input
         #new_function
--- sdk/math/wrappers.rs
+++ sdk/math/wrappers.rs
@@ -1,15 +1,15 @@
-// use crate::clamm::{
-//     calculate_amount_delta, calculate_max_liquidity_per_tick, calculate_min_amount_out, check_tick,
-//     check_ticks, compute_swap_step, get_delta_x, get_delta_y, get_next_sqrt_price_from_input,
-//     get_next_sqrt_price_from_output, get_next_sqrt_price_x_up, get_next_sqrt_price_y_down,
-//     is_enough_amount_to_change_price, SwapResult,
-// };
-// use crate::types::{
-//     liquidity::Liquidity, percentage::Percentage, sqrt_price::SqrtPrice, token_amount::TokenAmount,
-// };
+use crate::clamm::{
+    calculate_amount_delta, calculate_max_liquidity_per_tick, calculate_min_amount_out, check_tick,
+    check_ticks, compute_swap_step, get_delta_x, get_delta_y, get_next_sqrt_price_from_input,
+    get_next_sqrt_price_from_output, get_next_sqrt_price_x_up, get_next_sqrt_price_y_down,
+    is_enough_amount_to_change_price, SwapResult,
+};
+use crate::types::{
+    liquidity::Liquidity, percentage::Percentage, sqrt_price::SqrtPrice, token_amount::TokenAmount,
+};
 
-// use crate::{convert, resolve, wasm_helpers::AmountDeltaResult};
-// use wasm_bindgen::prelude::*;
+use crate::{convert, resolve, wasm_helpers::AmountDeltaResult};
+use wasm_bindgen::prelude::*;
 
 // #[wasm_bindgen(js_name = "getDeltaY")]
 // pub fn wrapped_get_delta_y(
@@ -49,197 +49,197 @@
 //     ))
 // }
 
-// #[wasm_bindgen(js_name = "getNextSqrtPriceFromInput")]
-// pub fn wrapped_get_next_sqrt_price_from_input(
-//     js_starting_sqrt_price: JsValue,
-//     js_liquidity: JsValue,
-//     js_amount: JsValue,
-//     js_x_to_y: JsValue,
-// ) -> Result<SqrtPrice, JsValue> {
-//     let starting_sqrt_price: SqrtPrice = convert!(js_starting_sqrt_price)?;
-//     let liquidity: Liquidity = convert!(js_liquidity)?;
-//     let amount: TokenAmount = convert!(js_amount)?;
-//     let x_to_y: bool = convert!(js_x_to_y)?;
-//     resolve!(get_next_sqrt_price_from_input(
-//         starting_sqrt_price,
-//         liquidity,
-//         amount,
-//         x_to_y
-//     ))
-// }
+#[wasm_bindgen(js_name = "getNextSqrtPriceFromInput")]
+pub fn wrapped_get_next_sqrt_price_from_input(
+    js_starting_sqrt_price: JsValue,
+    js_liquidity: JsValue,
+    js_amount: JsValue,
+    js_x_to_y: JsValue,
+) -> Result<SqrtPrice, JsValue> {
+    let starting_sqrt_price: SqrtPrice = convert!(js_starting_sqrt_price)?;
+    let liquidity: Liquidity = convert!(js_liquidity)?;
+    let amount: TokenAmount = convert!(js_amount)?;
+    let x_to_y: bool = convert!(js_x_to_y)?;
+    resolve!(get_next_sqrt_price_from_input(
+        starting_sqrt_price,
+        liquidity,
+        amount,
+        x_to_y
+    ))
+}
 
-// #[wasm_bindgen(js_name = "getNextSqrtPriceFromOutput")]
-// pub fn wrapped_get_next_sqrt_price_from_output(
-//     js_starting_sqrt_price: JsValue,
-//     js_liquidity: JsValue,
-//     js_amount: JsValue,
-//     js_x_to_y: JsValue,
-// ) -> Result<SqrtPrice, JsValue> {
-//     let starting_sqrt_price: SqrtPrice = convert!(js_starting_sqrt_price)?;
-//     let liquidity: Liquidity = convert!(js_liquidity)?;
-//     let amount: TokenAmount = convert!(js_amount)?;
-//     let x_to_y: bool = convert!(js_x_to_y)?;
-//     resolve!(get_next_sqrt_price_from_output(
-//         starting_sqrt_price,
-//         liquidity,
-//         amount,
-//         x_to_y
-//     ))
-// }
+#[wasm_bindgen(js_name = "getNextSqrtPriceFromOutput")]
+pub fn wrapped_get_next_sqrt_price_from_output(
+    js_starting_sqrt_price: JsValue,
+    js_liquidity: JsValue,
+    js_amount: JsValue,
+    js_x_to_y: JsValue,
+) -> Result<SqrtPrice, JsValue> {
+    let starting_sqrt_price: SqrtPrice = convert!(js_starting_sqrt_price)?;
+    let liquidity: Liquidity = convert!(js_liquidity)?;
+    let amount: TokenAmount = convert!(js_amount)?;
+    let x_to_y: bool = convert!(js_x_to_y)?;
+    resolve!(get_next_sqrt_price_from_output(
+        starting_sqrt_price,
+        liquidity,
+        amount,
+        x_to_y
+    ))
+}
 
-// #[wasm_bindgen(js_name = "getNextSqrtPriceXUp")]
-// pub fn wrapped_get_next_sqrt_price_x_up(
-//     js_starting_sqrt_price: JsValue,
-//     js_liquidity: JsValue,
-//     js_x: JsValue,
-//     js_add_x: JsValue,
-// ) -> Result<SqrtPrice, JsValue> {
-//     let starting_sqrt_price: SqrtPrice = convert!(js_starting_sqrt_price)?;
-//     let liquidity: Liquidity = convert!(js_liquidity)?;
-//     let x: TokenAmount = convert!(js_x)?;
-//     let add_x: bool = convert!(js_add_x)?;
-//     resolve!(get_next_sqrt_price_x_up(
-//         starting_sqrt_price,
-//         liquidity,
-//         x,
-//         add_x
-//     ))
-// }
+#[wasm_bindgen(js_name = "getNextSqrtPriceXUp")]
+pub fn wrapped_get_next_sqrt_price_x_up(
+    js_starting_sqrt_price: JsValue,
+    js_liquidity: JsValue,
+    js_x: JsValue,
+    js_add_x: JsValue,
+) -> Result<SqrtPrice, JsValue> {
+    let starting_sqrt_price: SqrtPrice = convert!(js_starting_sqrt_price)?;
+    let liquidity: Liquidity = convert!(js_liquidity)?;
+    let x: TokenAmount = convert!(js_x)?;
+    let add_x: bool = convert!(js_add_x)?;
+    resolve!(get_next_sqrt_price_x_up(
+        starting_sqrt_price,
+        liquidity,
+        x,
+        add_x
+    ))
+}
 
-// #[wasm_bindgen(js_name = "getNextSqrtPriceYDown")]
-// pub fn wrapped_get_next_sqrt_price_y_down(
-//     js_starting_sqrt_price: JsValue,
-//     js_liquidity: JsValue,
-//     js_y: JsValue,
-//     js_add_y: JsValue,
-// ) -> Result<SqrtPrice, JsValue> {
-//     let starting_sqrt_price: SqrtPrice = convert!(js_starting_sqrt_price)?;
-//     let liquidity: Liquidity = convert!(js_liquidity)?;
-//     let y: TokenAmount = convert!(js_y)?;
-//     let add_y: bool = convert!(js_add_y)?;
-//     resolve!(get_next_sqrt_price_y_down(
-//         starting_sqrt_price,
-//         liquidity,
-//         y,
-//         add_y
-//     ))
-// }
+#[wasm_bindgen(js_name = "getNextSqrtPriceYDown")]
+pub fn wrapped_get_next_sqrt_price_y_down(
+    js_starting_sqrt_price: JsValue,
+    js_liquidity: JsValue,
+    js_y: JsValue,
+    js_add_y: JsValue,
+) -> Result<SqrtPrice, JsValue> {
+    let starting_sqrt_price: SqrtPrice = convert!(js_starting_sqrt_price)?;
+    let liquidity: Liquidity = convert!(js_liquidity)?;
+    let y: TokenAmount = convert!(js_y)?;
+    let add_y: bool = convert!(js_add_y)?;
+    resolve!(get_next_sqrt_price_y_down(
+        starting_sqrt_price,
+        liquidity,
+        y,
+        add_y
+    ))
+}
 
-// #[wasm_bindgen(js_name = "calculateAmountDelta")]
-// pub fn wrapped_calculate_amount_delta(
-//     js_current_tick_index: JsValue,
-//     js_current_sqrt_price: JsValue,
-//     js_liquidity_delta: JsValue,
-//     js_liquidity_sign: JsValue,
-//     js_upper_tick: JsValue,
-//     js_lower_tick: JsValue,
-// ) -> Result<AmountDeltaResult, JsValue> {
-//     let current_tick_index: i32 = convert!(js_current_tick_index)?;
-//     let current_sqrt_price: SqrtPrice = convert!(js_current_sqrt_price)?;
-//     let liquidity_delta: Liquidity = convert!(js_liquidity_delta)?;
-//     let liquidity_sign: bool = convert!(js_liquidity_sign)?;
-//     let upper_tick: i32 = convert!(js_upper_tick)?;
-//     let lower_tick: i32 = convert!(js_lower_tick)?;
-//     match calculate_amount_delta(
-//         current_tick_index,
-//         current_sqrt_price,
-//         liquidity_delta,
-//         liquidity_sign,
-//         upper_tick,
-//         lower_tick,
-//     ) {
-//         Ok(result) => Ok(AmountDeltaResult {
-//             x: result.0,
-//             y: result.1,
-//             update_liquidity: result.2,
-//         }),
-//         Err(error) => Err(JsValue::from_str(&error.cause)),
-//     }
-// }
+#[wasm_bindgen(js_name = "calculateAmountDelta")]
+pub fn wrapped_calculate_amount_delta(
+    js_current_tick_index: JsValue,
+    js_current_sqrt_price: JsValue,
+    js_liquidity_delta: JsValue,
+    js_liquidity_sign: JsValue,
+    js_upper_tick: JsValue,
+    js_lower_tick: JsValue,
+) -> Result<AmountDeltaResult, JsValue> {
+    let current_tick_index: i32 = convert!(js_current_tick_index)?;
+    let current_sqrt_price: SqrtPrice = convert!(js_current_sqrt_price)?;
+    let liquidity_delta: Liquidity = convert!(js_liquidity_delta)?;
+    let liquidity_sign: bool = convert!(js_liquidity_sign)?;
+    let upper_tick: i32 = convert!(js_upper_tick)?;
+    let lower_tick: i32 = convert!(js_lower_tick)?;
+    match calculate_amount_delta(
+        current_tick_index,
+        current_sqrt_price,
+        liquidity_delta,
+        liquidity_sign,
+        upper_tick,
+        lower_tick,
+    ) {
+        Ok(result) => Ok(AmountDeltaResult {
+            x: result.0,
+            y: result.1,
+            update_liquidity: result.2,
+        }),
+        Err(error) => Err(JsValue::from_str(&error.cause)),
+    }
+}
 
-// #[wasm_bindgen(js_name = "isEnoughAmountToChangePrice")]
-// pub fn wrapped_is_enough_to_change_price(
-//     js_amount: JsValue,
-//     js_starting_sqrt_price: JsValue,
-//     js_liquidity: JsValue,
-//     js_fee: JsValue,
-//     js_by_amount_in: JsValue,
-//     js_x_to_y: JsValue,
-// ) -> Result<bool, JsValue> {
-//     let amount: TokenAmount = convert!(js_amount)?;
-//     let starting_sqrt_price: SqrtPrice = convert!(js_starting_sqrt_price)?;
-//     let liquidity: Liquidity = convert!(js_liquidity)?;
-//     let fee: Percentage = convert!(js_fee)?;
-//     let by_amount_in: bool = convert!(js_by_amount_in)?;
-//     let x_to_y: bool = convert!(js_x_to_y)?;
-//     resolve!(is_enough_amount_to_change_price(
-//         amount,
-//         starting_sqrt_price,
-//         liquidity,
-//         fee,
-//         by_amount_in,
-//         x_to_y
-//     ))
-// }
+#[wasm_bindgen(js_name = "isEnoughAmountToChangePrice")]
+pub fn wrapped_is_enough_to_change_price(
+    js_amount: JsValue,
+    js_starting_sqrt_price: JsValue,
+    js_liquidity: JsValue,
+    js_fee: JsValue,
+    js_by_amount_in: JsValue,
+    js_x_to_y: JsValue,
+) -> Result<bool, JsValue> {
+    let amount: TokenAmount = convert!(js_amount)?;
+    let starting_sqrt_price: SqrtPrice = convert!(js_starting_sqrt_price)?;
+    let liquidity: Liquidity = convert!(js_liquidity)?;
+    let fee: Percentage = convert!(js_fee)?;
+    let by_amount_in: bool = convert!(js_by_amount_in)?;
+    let x_to_y: bool = convert!(js_x_to_y)?;
+    resolve!(is_enough_amount_to_change_price(
+        amount,
+        starting_sqrt_price,
+        liquidity,
+        fee,
+        by_amount_in,
+        x_to_y
+    ))
+}
 
-// #[wasm_bindgen(js_name = "calculateMaxLiquidityPerTick")]
-// pub fn wrapped_calculate_max_liquidity_per_tick(
-//     js_tick_spacing: JsValue,
-// ) -> Result<Liquidity, JsValue> {
-//     let tick_spacing: u16 = convert!(js_tick_spacing)?;
-//     Ok(calculate_max_liquidity_per_tick(tick_spacing))
-// }
+#[wasm_bindgen(js_name = "calculateMaxLiquidityPerTick")]
+pub fn wrapped_calculate_max_liquidity_per_tick(
+    js_tick_spacing: JsValue,
+) -> Result<Liquidity, JsValue> {
+    let tick_spacing: u16 = convert!(js_tick_spacing)?;
+    Ok(calculate_max_liquidity_per_tick(tick_spacing))
+}
 
-// #[wasm_bindgen(js_name = "checkTicks")]
-// pub fn wrapped_check_ticks(
-//     js_tick_lower: JsValue,
-//     js_tick_upper: JsValue,
-//     js_tick_spacing: JsValue,
-// ) -> Result<(), JsValue> {
-//     let tick_lower: i32 = convert!(js_tick_lower)?;
-//     let tick_upper: i32 = convert!(js_tick_upper)?;
-//     let tick_spacing: u16 = convert!(js_tick_spacing)?;
-//     resolve!(check_ticks(tick_lower, tick_upper, tick_spacing))
-// }
+#[wasm_bindgen(js_name = "checkTicks")]
+pub fn wrapped_check_ticks(
+    js_tick_lower: JsValue,
+    js_tick_upper: JsValue,
+    js_tick_spacing: JsValue,
+) -> Result<(), JsValue> {
+    let tick_lower: i32 = convert!(js_tick_lower)?;
+    let tick_upper: i32 = convert!(js_tick_upper)?;
+    let tick_spacing: u16 = convert!(js_tick_spacing)?;
+    resolve!(check_ticks(tick_lower, tick_upper, tick_spacing))
+}
 
-// #[wasm_bindgen(js_name = "checkTick")]
-// pub fn wrapped_check_tick(js_tick_index: JsValue, js_tick_spacing: JsValue) -> Result<(), JsValue> {
-//     let tick_index: i32 = convert!(js_tick_index)?;
-//     let tick_spacing: u16 = convert!(js_tick_spacing)?;
-//     resolve!(check_tick(tick_index, tick_spacing))
-// }
+#[wasm_bindgen(js_name = "checkTick")]
+pub fn wrapped_check_tick(js_tick_index: JsValue, js_tick_spacing: JsValue) -> Result<(), JsValue> {
+    let tick_index: i32 = convert!(js_tick_index)?;
+    let tick_spacing: u16 = convert!(js_tick_spacing)?;
+    resolve!(check_tick(tick_index, tick_spacing))
+}
 
-// #[wasm_bindgen(js_name = "calculateMinAmountOut")]
-// pub fn wrapped_calculate_min_amount_out(
-//     js_expected_amount_out: JsValue,
-//     js_slippage: JsValue,
-// ) -> Result<TokenAmount, JsValue> {
-//     let expected_amount_out: TokenAmount = convert!(js_expected_amount_out)?;
-//     let slippage: Percentage = convert!(js_slippage)?;
-//     Ok(calculate_min_amount_out(expected_amount_out, slippage))
-// }
+#[wasm_bindgen(js_name = "calculateMinAmountOut")]
+pub fn wrapped_calculate_min_amount_out(
+    js_expected_amount_out: JsValue,
+    js_slippage: JsValue,
+) -> Result<TokenAmount, JsValue> {
+    let expected_amount_out: TokenAmount = convert!(js_expected_amount_out)?;
+    let slippage: Percentage = convert!(js_slippage)?;
+    Ok(calculate_min_amount_out(expected_amount_out, slippage))
+}
 
-// #[wasm_bindgen(js_name = "computeSwapStep")]
-// pub fn wrapped_compute_swap_step(
-//     js_current_sqrt_price: JsValue,
-//     js_target_sqrt_price: JsValue,
-//     js_liquidity: JsValue,
-//     js_amount: JsValue,
-//     js_by_amount_in: JsValue,
-//     js_fee: JsValue,
-// ) -> Result<SwapResult, JsValue> {
-//     let current_sqrt_price: SqrtPrice = convert!(js_current_sqrt_price)?;
-//     let target_sqrt_price: SqrtPrice = convert!(js_target_sqrt_price)?;
-//     let liquidity: Liquidity = convert!(js_liquidity)?;
-//     let amount: TokenAmount = convert!(js_amount)?;
-//     let by_amount_in: bool = convert!(js_by_amount_in)?;
-//     let fee: Percentage = convert!(js_fee)?;
-//     resolve!(compute_swap_step(
-//         current_sqrt_price,
-//         target_sqrt_price,
-//         liquidity,
-//         amount,
-//         by_amount_in,
-//         fee
-//     ))
-// }
+#[wasm_bindgen(js_name = "computeSwapStep")]
+pub fn wrapped_compute_swap_step(
+    js_current_sqrt_price: JsValue,
+    js_target_sqrt_price: JsValue,
+    js_liquidity: JsValue,
+    js_amount: JsValue,
+    js_by_amount_in: JsValue,
+    js_fee: JsValue,
+) -> Result<SwapResult, JsValue> {
+    let current_sqrt_price: SqrtPrice = convert!(js_current_sqrt_price)?;
+    let target_sqrt_price: SqrtPrice = convert!(js_target_sqrt_price)?;
+    let liquidity: Liquidity = convert!(js_liquidity)?;
+    let amount: TokenAmount = convert!(js_amount)?;
+    let by_amount_in: bool = convert!(js_by_amount_in)?;
+    let fee: Percentage = convert!(js_fee)?;
+    resolve!(compute_swap_step(
+        current_sqrt_price,
+        target_sqrt_price,
+        liquidity,
+        amount,
+        by_amount_in,
+        fee
+    ))
+}
