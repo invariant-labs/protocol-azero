38671ff1b5109f50d1f875a80b7b172ed9f0e8f9
Sniezka <dygons14@gmail.com>
2024-01-24 14:58:36 +0100

    removed only

--- sdk/tests/wasm.test.ts
+++ sdk/tests/wasm.test.ts
@@ -1,6 +1,6 @@
 import { assert } from 'chai'
-import { SqrtPrice } from 'clamm/index.js'
-describe.only('wasm', async () => {
+import { SqrtPrice } from 'clamm'
+describe('wasm', async () => {
   it('should load wasm', async () => {
     const clammSqrtPrice: SqrtPrice = 0n
     assert.equal(clammSqrtPrice, 0n)
