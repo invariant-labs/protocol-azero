d22a57d3df4cd10f64c8a8fdce93992481292eb1
Wojciech <cichockiwojciech97@gmail.com>
2024-01-25 03:19:09 +0100

    Update tests.sh script - exit with test_status

--- sdk/tests.sh
+++ sdk/tests.sh
@@ -5,4 +5,7 @@ npm run test &
 test_pid=$!
 
 wait $test_pid
-ps aux | grep substrate-contracts-node | tr -s ' ' | cut -d ' ' -f 2 | head -n 1 | xargs -r kill -9
+test_status=$?
+
+npm run node:stop
+exit $test_status
