b141863108a12d84d856d17c39fff99d52a3c041
Sniezka <dygons14@gmail.com>
2024-01-28 23:40:33 +0100

    Hard coded value

--- sdk/src/utils.ts
+++ sdk/src/utils.ts
@@ -32,7 +32,7 @@ import { LiquidityBreakpoint, Query, Tx, TxResult } from './schema.js'
 export const initPolkadotApi = async (network: Network): Promise<ApiPromise> => {
   if (network === Network.Local) {
     const localId = LOCAL
-    const wsProvider = new WsProvider(localId)
+    const wsProvider = new WsProvider('ws://localhost:9944')
     const api = await ApiPromise.create({ provider: wsProvider })
     await api.isReady
     return api
