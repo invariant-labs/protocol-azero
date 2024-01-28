10d96341ea37c9d4dca39473be7dd482427a8f21
Sniezka <dygons14@gmail.com>
2024-01-28 22:19:58 +0100

    Update

--- sdk/src/consts.ts
+++ sdk/src/consts.ts
@@ -1,6 +1,6 @@
 export const MAX_REF_TIME = 259_058_343_000
 export const DEFAULT_REF_TIME = 1250000000000
 export const DEFAULT_PROOF_SIZE = 1250000000000
-export const CHAIN = 'alephzero - testnet'
+export const CHAIN = 'alephzero-testnet'
 export const LOCAL = 'ws://localhost:9944'
 export const WAZERO_ADDRESS = '5EFDb7mKbougLtr5dnwd5KDfZ3wK55JPGPLiryKq4uRMPR46'
