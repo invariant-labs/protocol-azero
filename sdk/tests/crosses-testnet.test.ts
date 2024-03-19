c5d74658eb92ff919b43ec32ecbcb459af64d004
Sniezka <dygons14@gmail.com>
2024-03-19 11:35:16 +0100

    Created test file

--- sdk/tests/crosses-testnet.test.ts
+++ sdk/tests/crosses-testnet.test.ts
@@ -7,7 +7,7 @@ import { PSP22 } from '../src/psp22.js'
 import { ContractOptions } from '../src/schema.js'
 import { initPolkadotApi, integerSafeCast, newFeeTier, newPoolKey } from '../src/utils.js'
 
-describe('testnet-crosses-limitations', async () => {
+describe.only('testnet-crosses-limitations', async () => {
   it('Validate limitation number of crosses in single atomic swap', async function () {
     this.timeout(2000000)
     const network = Network.Testnet
