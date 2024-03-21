503e2d2baac0f2dda814a0a12d02ecdfea94f868
zielvna <zielvna@gmail.com>
2024-03-21 18:29:11 +0100

    fix tests

--- sdk/tests/limits.test.ts
+++ sdk/tests/limits.test.ts
@@ -1,33 +1,36 @@
 import { Keyring } from '@polkadot/api'
 import { assert } from 'chai'
+import { DEFAULT_PROOF_SIZE, MAX_REF_TIME } from '../src/consts'
 import { Invariant } from '../src/invariant'
 import { Network } from '../src/network'
 import { PSP22 } from '../src/psp22'
 import { initPolkadotApi, newFeeTier, newPoolKey } from '../src/utils'
-import { toPercentage } from '../src/wasm/pkg/invariant_a0_wasm.js'
-
-const api = await initPolkadotApi(Network.Testnet)
-
-const keyring = new Keyring({ type: 'sr25519' })
-const account = await keyring.addFromUri('//Alice')
-const feeTier = newFeeTier(10000000000n, 1n)
-
-let invariant = await Invariant.deploy(api, Network.Testnet, account, toPercentage(1n, 2n))
-let token0Address = await PSP22.deploy(api, account, 1000000000000n, 'Coin', 'COIN', 0n)
-let token1Address = await PSP22.deploy(api, account, 1000000000000n, 'Coin', 'COIN', 0n)
-let poolKey = newPoolKey(token0Address, token1Address, feeTier)
-
-const psp22 = await PSP22.load(api, Network.Testnet, token0Address)
+import { toPercentage } from '../src/wasm/pkg/invariant_a0_wasm'
 
 describe('limits', async () => {
-  beforeEach(async function () {
-    this.timeout(60000)
+  it('storage limit test', async function () {
+    this.timeout(1000000000000)
 
-    invariant = await Invariant.deploy(api, Network.Testnet, account, 10000000000n)
-    token0Address = await PSP22.deploy(api, account, 1000000000n, 'Coin', 'COIN', 0n)
-    token1Address = await PSP22.deploy(api, account, 1000000000n, 'Coin', 'COIN', 0n)
+    const api = await initPolkadotApi(Network.Testnet)
 
-    poolKey = newPoolKey(token0Address, token1Address, feeTier)
+    const keyring = new Keyring({ type: 'sr25519' })
+    const account = await keyring.addFromUri('//Alice')
+    const feeTier = newFeeTier(10000000000n, 1n)
+
+    const invariant = await Invariant.deploy(api, Network.Testnet, account, toPercentage(1n, 2n), {
+      storageDepositLimit: null,
+      refTime: MAX_REF_TIME,
+      proofSize: DEFAULT_PROOF_SIZE
+    })
+    const token0Address = await PSP22.deploy(api, account, 1000000000000n, 'Coin', 'COIN', 0n)
+    const token1Address = await PSP22.deploy(api, account, 1000000000000n, 'Coin', 'COIN', 0n)
+    const poolKey = newPoolKey(token0Address, token1Address, feeTier)
+
+    const psp22 = await PSP22.load(api, Network.Testnet, token0Address, {
+      storageDepositLimit: null,
+      refTime: MAX_REF_TIME,
+      proofSize: DEFAULT_PROOF_SIZE
+    })
 
     await invariant.addFeeTier(account, feeTier)
 
@@ -37,10 +40,6 @@ describe('limits', async () => {
     await psp22.approve(account, invariant.contract.address.toString(), 10000000000n)
     await psp22.setContractAddress(token1Address)
     await psp22.approve(account, invariant.contract.address.toString(), 10000000000n)
-  })
-
-  it('storage limit test', async function () {
-    this.timeout(1000000000000)
 
     for (let i = 1; i <= 1619; i++) {
       console.log(i)
