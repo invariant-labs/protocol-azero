6cffd8216da6c012ffaeb6d0f04f6c2c7dc5f56f
zielvna <zielvna@gmail.com>
2024-03-25 10:53:52 +0100

    update create pool tests

--- src/e2e/mod.rs
+++ src/e2e/mod.rs
@@ -3,7 +3,7 @@ pub mod change_fee_receiver;
 pub mod change_protocol_fee;
 pub mod claim;
 pub mod constructor;
-// pub mod create_pool;
+pub mod create_pool;
 // pub mod cross;
 // pub mod cross_both_side;
 // pub mod get_liquidity_ticks;
