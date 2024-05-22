24db44cf1df2da2e6cd03c03f9451319acddb213
none00y <mich_smyk@wp.pl>
2024-05-22 13:58:45 +0200

    add swap simulation to sdk usage

--- sdk-usage/src/swap.ts
+++ sdk-usage/src/swap.ts
@@ -3,10 +3,17 @@ import {
   Keyring,
   Network,
   PSP22,
+  PoolKey,
+  SwapEvent,
   TESTNET_INVARIANT_ADDRESS,
   calculateFee,
-  initPolkadotApi
+  getMinTick,
+  initPolkadotApi,
+  simulateInvariantSwap,
+  positionToTick
 } from '@invariant-labs/a0-sdk'
+import { CHUNK_SIZE } from '@invariant-labs/a0-sdk/target/consts.js'
+import assert from 'assert'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -19,7 +26,7 @@ const main = async () => {
   const mnemonic = process.env.DEPLOYER_MNEMONIC ?? ''
   const account = keyring.addFromMnemonic(mnemonic)
 
-  const POSITION_ID = 14n
+  const POSITION_ID = 0n
   const SWAP_AMOUNT = 1000000n
 
   const invariant = await Invariant.load(api, network, TESTNET_INVARIANT_ADDRESS, {
@@ -44,8 +51,76 @@ const main = async () => {
   await psp22.mint(account, SWAP_AMOUNT)
   await psp22.approve(account, TESTNET_INVARIANT_ADDRESS, SWAP_AMOUNT)
 
-  await invariant.swap(account, positionBefore.poolKey, true, SWAP_AMOUNT, true, 0n)
-  await invariant.swap(account, positionBefore.poolKey, false, SWAP_AMOUNT, true, 2n ** 128n - 1n)
+  const protocolFee = await invariant.getProtocolFee()
+  const {
+    tickmap: tickmapBeforeFirstSwap,
+    ticks: ticksBeforeFirstSwap,
+    pool: poolBeforeFirstSwap
+  } = await getPoolState(invariant, positionBefore.poolKey)
+
+  const firstSimualtion = simulateInvariantSwap(
+    tickmapBeforeFirstSwap,
+    protocolFee,
+    positionBefore.poolKey.feeTier,
+    poolBeforeFirstSwap,
+    ticksBeforeFirstSwap,
+    true,
+    SWAP_AMOUNT,
+    true,
+    0n
+  )
+  const firstSwapResult = await invariant.swap(
+    account,
+    positionBefore.poolKey,
+    true,
+    SWAP_AMOUNT,
+    true,
+    0n
+  )
+  const firstSwapEvent = firstSwapResult.events[0] as SwapEvent
+  assert(firstSimualtion.globalInsufficientLiquidity === false)
+  assert(firstSimualtion.maxTicksCrossed === false)
+  assert(firstSimualtion.stateOutdated === false)
+  assert(firstSimualtion.amountIn == firstSwapEvent.amountIn)
+  assert(firstSimualtion.amountOut == firstSwapEvent.amountOut)
+  assert(firstSimualtion.crossedTicks.length === 0)
+  assert(firstSimualtion.startSqrtPrice === firstSwapEvent.startSqrtPrice)
+  assert(firstSimualtion.targetSqrtPrice === firstSwapEvent.targetSqrtPrice)
+
+  const {
+    tickmap: tickmapBeforeSecondSwap,
+    ticks: ticksBeforeSecondSwap,
+    pool: poolBeforeSecondSwap
+  } = await getPoolState(invariant, positionBefore.poolKey)
+
+  const secondSimulation = simulateInvariantSwap(
+    tickmapBeforeSecondSwap,
+    protocolFee,
+    positionBefore.poolKey.feeTier,
+    poolBeforeSecondSwap,
+    ticksBeforeSecondSwap,
+    false,
+    SWAP_AMOUNT,
+    true,
+    2n ** 128n - 1n
+  )
+  const secondSwapResult = await invariant.swap(
+    account,
+    positionBefore.poolKey,
+    false,
+    SWAP_AMOUNT,
+    true,
+    2n ** 128n - 1n
+  )
+  const secondSwapEvent = secondSwapResult.events[0] as SwapEvent
+  assert(secondSimulation.globalInsufficientLiquidity === false)
+  assert(secondSimulation.maxTicksCrossed === false)
+  assert(secondSimulation.stateOutdated === false)
+  assert(secondSimulation.amountIn === secondSwapEvent.amountIn)
+  assert(secondSimulation.amountOut === secondSwapEvent.amountOut)
+  assert(secondSimulation.crossedTicks.length === 0)
+  assert(secondSimulation.startSqrtPrice === secondSwapEvent.startSqrtPrice)
+  assert(secondSimulation.targetSqrtPrice === secondSwapEvent.targetSqrtPrice)
 
   const pool = await invariant.getPool(
     positionBefore.poolKey.tokenX,
@@ -60,5 +135,23 @@ const main = async () => {
 
   process.exit(0)
 }
+const getPoolState = async (invariant: Invariant, poolKey: PoolKey) => {
+  console.log(poolKey)
+  const tickmap = await invariant.getFullTickmap(poolKey)
+  const promises = []
+  for (const [chunkIndex, chunk] of tickmap.bitmap.entries()) {
+    for (let bit = 0n; bit < CHUNK_SIZE; bit++) {
+      const checkedBit = chunk & (1n << bit)
+      if (checkedBit) {
+        const tickIndex = positionToTick(chunkIndex, bit, poolKey.feeTier.tickSpacing)
+        promises.push(invariant.getTick(poolKey, tickIndex))
+      }
+    }
+  }
 
+  const ticks = await Promise.all(promises)
+
+  const pool = await invariant.getPool(poolKey.tokenX, poolKey.tokenY, poolKey.feeTier)
+  return { tickmap, ticks, pool }
+}
 main()
