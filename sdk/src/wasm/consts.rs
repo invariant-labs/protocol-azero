18925c7267c88b1642bfd11e75800243beb78231
zielvna <zielvna@gmail.com>
2024-06-07 10:16:34 +0200

    add get all pools for pair entrypoint limit

--- sdk/src/wasm/consts.rs
+++ sdk/src/wasm/consts.rs
@@ -20,6 +20,9 @@ pub const LIQUIDITY_TICK_LIMIT: usize = MAX_RESULT_SIZE / (32 + 128 + 8);
 
 pub const MAX_POOL_KEYS_RETURNED: u16 = 220;
 
+pub const MAX_POOL_PAIRS_RETURNED: usize =
+    MAX_RESULT_SIZE / (128 + 128 + 32 + 128 + 128 + 128 + 128 + 64 + 64 + 32 + 64 + 16);
+
 #[wasm_wrapper]
 pub fn get_global_max_sqrt_price() -> u128 {
     MAX_SQRT_PRICE
@@ -66,4 +69,9 @@ pub fn get_liquidity_ticks_limit() -> u64 {
 #[wasm_wrapper]
 pub fn get_max_pool_keys_returned() -> u16 {
     MAX_POOL_KEYS_RETURNED
+}
+
+#[wasm_wrapper]
+pub fn get_max_pool_pairs_returned() -> u64 {
+    MAX_POOL_PAIRS_RETURNED as u64
 }
