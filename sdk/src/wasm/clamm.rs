5efdedad53c3c3fe17f04aa6f98a24e6f97cbeb6
zielvna <zielvna@gmail.com>
2024-06-14 13:14:37 +0200

    update get next sqrt price y down function in wasm and add more max token tests to swap simulation

--- sdk/src/wasm/clamm.rs
+++ sdk/src/wasm/clamm.rs
@@ -247,11 +247,11 @@ pub fn get_next_sqrt_price_x_up(
     let price_delta = ok_or_mark_trace!(SqrtPrice::checked_from_decimal_to_value(liquidity)
         .map_err(|_| err!("extending liquidity overflow")))?;
 
-    let denominator = ok_or_mark_trace!(match add_x {
+    let denominator = (match add_x {
         true => price_delta.checked_add(starting_sqrt_price.big_mul_to_value(x)),
         false => price_delta.checked_sub(starting_sqrt_price.big_mul_to_value(x)),
-    }
-    .ok_or_else(|| err!("big_liquidity -/+ sqrt_price * x")))?; // never should be triggered
+    })
+    .unwrap_or(U256::MAX);
 
     ok_or_mark_trace!(SqrtPrice::checked_big_div_values_up(
         starting_sqrt_price.big_mul_to_value_up(liquidity),
@@ -273,15 +273,11 @@ pub fn get_next_sqrt_price_y_down(
 
     if add_y {
         let quotient = SqrtPrice::checked_big_div_values(numerator, denominator)
-            .unwrap_or(SqrtPrice::new(2u128.pow(64) - 1));
-        // let quotient =
-        //     ok_or_mark_trace!(SqrtPrice::checked_big_div_values(numerator, denominator))?;
+            .unwrap_or(SqrtPrice::new(MAX_SQRT_PRICE - 1));
         from_result!(starting_sqrt_price.checked_add(quotient))
     } else {
         let quotient = SqrtPrice::checked_big_div_values_up(numerator, denominator)
-            .unwrap_or(SqrtPrice::new(2u128.pow(64) - 1));
-        // let quotient =
-        //     ok_or_mark_trace!(SqrtPrice::checked_big_div_values_up(numerator, denominator))?;
+            .unwrap_or(SqrtPrice::new(MIN_SQRT_PRICE + 1));
         from_result!(starting_sqrt_price.checked_sub(quotient))
     }
 }
