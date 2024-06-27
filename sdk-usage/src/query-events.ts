7c4c5f6d284308ed20532c8a0dfdba4f78028188
zielvna <zielvna@gmail.com>
2024-06-27 13:45:31 +0200

    refactor query events script

--- sdk-usage/src/query-events.ts
+++ sdk-usage/src/query-events.ts
@@ -1,15 +1,11 @@
-import { Invariant } from '@invariant-labs/a0-sdk'
+import { Invariant, TESTNET_INVARIANT_ADDRESS } from '@invariant-labs/a0-sdk'
 import { Network, initPolkadotApi } from '@invariant-labs/a0-sdk'
 import { parseEvent } from '@invariant-labs/a0-sdk/target/utils.js'
 
 const main = async () => {
-  const api = await initPolkadotApi(Network.Testnet)
-  const invariant = await Invariant.load(
-    api,
-    Network.Testnet,
-    '5GAHRxhgJEcadjfGpLgNNb6gN4794GHX6Z9zjq6JfmYkBAXt'
-  )
-  const abi = invariant.abi
+  const network = Network.Testnet
+  const api = await initPolkadotApi(network)
+  const invariant = await Invariant.load(api, network, TESTNET_INVARIANT_ADDRESS)
 
   const blockNumber = await api.query.system.number()
   for (let i = 0; i < 100; i++) {
@@ -25,7 +21,7 @@ const main = async () => {
         const [account_id] = event.data
 
         if (account_id.toString() === invariant.contract.address.toString()) {
-          const decoded = abi.decodeEvent(record as any)
+          const decoded = invariant.abi.decodeEvent(record as any)
           console.log('Invariant event: ', parseEvent(decoded))
         }
       }
