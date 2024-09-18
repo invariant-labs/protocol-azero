b280ea6a050a7a3ecd5d874aacf67ce5c2ed0461
none00y <mich_smyk@wp.pl>
2024-09-18 12:00:57 +0200

    deploy mainnet

--- sdk/src/consts.ts
+++ sdk/src/consts.ts
@@ -44,32 +44,32 @@ export const WAZERO_ADDRESS = {
 
 export const INVARIANT_ADDRESS = {
   [Network.Testnet]: '5GmvfVAmALxttyzgRg62R5puhzRwD6N58nAdFmZV9gQG1RGK',
-  [Network.Mainnet]: '5CvocBcChFccUkNGZpYf1mThQQDaY7ZxXEmdTXbTLqt1SaYQ',
+  [Network.Mainnet]: '5CaXNdnWMSAX5ivhU2mTS2ukVCpVAeXS3MzMXNidELrawhTu',
   [Network.Local]: ''
 }
 export const BTC_ADDRESS = {
   [Network.Testnet]: '5Cvyd9GtVUSuigFkAjimYqxpqUfzXaht3rKMA9WjQBDeeeiu',
-  [Network.Mainnet]: '5HW9QeCifdKt8gXwXVSE8z56njDQBhGfses1KJNFL68qius9',
+  [Network.Mainnet]: '5EVzJwtb8yeFksbkXsuD49RZeYbzTRUKSyZ8y8iu1bMqrfvS',
   [Network.Local]: ''
 }
 export const ETH_ADDRESS = {
   [Network.Testnet]: '5CvotjnedNwsJ2thPjiqE4MEsJXhfQB99uC2fkCKnQkgJqj1',
-  [Network.Mainnet]: '5EEzffpXkfYKkdtmqNh9UNctYTjmbi9GfKKAWRTKKFh6F1FU',
+  [Network.Mainnet]: '5Gu1E9FZrKP2vpLfBXiaRQQjeGRn4ssxj8DrXyFQCkR3N2fc',
   [Network.Local]: ''
 }
 export const USDC_ADDRESS = {
   [Network.Testnet]: '5FaDyzc3K1TtYiuwjcqJzYsXn4NG93Yrimz1dTXPY6HyVFp4',
-  [Network.Mainnet]: '5GDsB8Qm6CAoBi7rmM6TCKMQQUg8CiRzuH9YVyfcrwDKWoqB',
+  [Network.Mainnet]: '5FjtfRqa2dMELrTuhEWd3XgTMXRfX4Sb4mKfRebWdvuVccJ5',
   [Network.Local]: ''
 }
 export const USDT_ADDRESS = {
   [Network.Testnet]: '5Gf9znZokPCkgczpsZDRYQCF7n9iCuWRzNgbTenPoQRHz33V',
-  [Network.Mainnet]: '5HX57YoV7h51NEKhpfXZAJk8RzLX4Uutp36S23RDMPZ424LY',
+  [Network.Mainnet]: '5FxenfueeATgv34qiVgpXkAdFSgV56AshpqwVzfPNGj6ozfA',
   [Network.Local]: ''
 }
 export const SOL_ADDRESS = {
   [Network.Testnet]: '5EDwDPeXY7kkLnTfWGjXex3n6gkp9swynm82hpL3LanW1MaH',
-  [Network.Mainnet]: '5F2xiTnahG1tFY3ZHyghh25JsjuCaamRnN7ddQjPEzwvdd3j',
+  [Network.Mainnet]: '5DzJvZpriJfdyvjFYUW7SbJDQHXs8PBGeytCz6DPgC6ofzmG',
   [Network.Local]: ''
 }
 
